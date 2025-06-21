21d4eec4bcc12f6911f6cc554bc0cf8f98e4a998
sirisha1697 <168277085+sirisha1697@users.noreply.github.com>
2025-06-21 14:51:38 +0530

    Add files via upload

---
+++ conditionalstatements.py
@@ -0,0 +1,19 @@
+"""
+Python conditonal statements are broadly divided into "3" Types
+1.Condtional /Decision Making statements
+2. Looping/Iterative Statements
+3. Jumping/Transfer Statements
+
+"""
+#Decision Making Statements
+"""
+Decison Making statements are used to check the conditions and execute them
+The decision Making statements are:
+1. if statement
+2. if else statement
+3. elif statement
+4. Nested if statement
+5. Shorthand if 
+6. Shorthand if else
+
+"""
